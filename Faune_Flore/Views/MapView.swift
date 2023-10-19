23b849d6ccaf01ad599849248989f91733fe1ab6
Thomas MESSIN <messin.thomas@hotmail.fr>
2023-10-19 16:14:39 +0200

    Création fichier MapView

---
+++ Faune_Flore/Views/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Faune_Flore
+//
+//  Created by Thomas MESSIN on 19/10/2023.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
