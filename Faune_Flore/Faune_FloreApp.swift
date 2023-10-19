2bc18538090ecc3efd684797561a6fcac27012bc
Enzo <enzomartolini@mailo.com>
2023-10-19 10:14:43 +0200

    Initial Commit

---
+++ Faune_Flore/Faune_FloreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Faune_FloreApp.swift
+//  Faune_Flore
+//
+//  Created by Travail on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Faune_FloreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
